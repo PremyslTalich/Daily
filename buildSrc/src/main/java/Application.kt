e4b056c11f59d11d5a6dd6a544e91eddf4b43f7a
Přemysl Talich <p.talich@gmail.com>
2023-02-13 00:15:35 +0100

    Release Daily v1.2

--- buildSrc/src/main/java/Application.kt
+++ buildSrc/src/main/java/Application.kt
@@ -3,7 +3,7 @@ object Application {
     const val namespace = id
 
     object Version {
-        const val name = "1.1"
+        const val name = "1.2"
         val code: Int
             get() = name.replace(".", "").toInt()
     }
